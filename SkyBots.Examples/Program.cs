85d42def30925b3d78ec8831c1e2bfce6963d3d1
LunarLife <ivan.andreychenko1502@gmail.com>
2023-12-22 03:15:32 +0100

    Interaction with chest inventory!

--- SkyBots.Examples/Program.cs
+++ SkyBots.Examples/Program.cs
@@ -2,11 +2,13 @@
 using SkyBots.Api.Bots;
 using SkyBots.Api.Components.Entities.Bots;
 using SkyBots.Api.Components.Entities.Bots.Navigate;
+using SkyBots.Api.Components.Entities.Bots.TaskMachine;
 using SkyBots.Api.Entities;
 using SkyBots.Api.Events.Bots;
 using SkyBots.Api.Events.Entities;
 using SkyBots.Api.Events.Inventories;
 using SkyBots.Api.Events.Inventories.SlotChange;
+using SkyBots.Api.Events.Tasks;
 using SkyBots.Api.Events.World;
 using SkyBots.Api.Jobs.Instructions;
 using SkyBots.Api.Mathematics;
@@ -24,8 +26,8 @@ public class Program : SkyProgram
     {
         if (result != AuthResult.Successfully) return;
         if (Island.FreeBots.Count == 0) Debug.Info("No available bots.");
-        var entity = Island.FreeBots[0];
-        var bot = entity.GetComponent<Bot>();
+        var bot = Island.FreeBots[0];
+        var entity = bot.Entity;
         Debug.Info($"Try to bind bot {entity.Id}");
         var botBindingResult = await bot.Bind(new BotBindArgs
         {
@@ -46,14 +48,6 @@ public class Program : SkyProgram
 
         Debug.Info("Binding: " + botBindingResult);
 
-        var target = new Vector3<float>(5f, 101, 5f);
-        Debug.Info($"Initial bot moving to {target};");
-        var task = entity.GetComponent<Navigator>().Navigate(new MoveArgs
-        {
-            Sprint = false,
-            Target = target
-        });
-        task.OnCompleted.AddListener(MoveCompleteHandler);
         var world = World;
         if (!world.IsLoaded)
         {
@@ -62,16 +56,32 @@ public class Program : SkyProgram
             Debug.Info("World loaded");
         }
 
-        Debug.Info(world.GetBlockAt(0, 100, 0).Type);
+        var navigator = entity.GetComponent<Navigator>();
+        var navigate = navigator.Navigate(new BotNavigateArgs
+        {
+            Target = new Vector3<int>(1, 101, 1),
+            Sprint = true
+        });
+        navigate.OnCompleted.AddListener(MoveCompletedHandler);
+        await navigate.WaitAsync();
+
+        await bot.InteractBlock(new Vector3<int>(0, 101, 0)).WaitAsync();
+        bot.OpenedInventory!.OnSlotChange.AddListener(args =>
+        {
+            Console.WriteLine($"Changed opened inventory {args.Inventory.Type} from {args.Old} to {args.New}.");
+        });
     }
 
     private void InventorySlotChangeHandler(BotInventorySlotChangeEventArgs args)
     {
-        Debug.Info($"Bot {args.Bot.Name} slot {args.Slot}changed from {args.Old} to {args.New}");
+        Debug.Info($"Bot {args.Bot.Name} slot {args.Slot} changed from {args.Old} to {args.New}");
     }
 
-    private void InventoryOpenHandler(InventoryOpenEventArgs args) => Debug.Info($"Bot {args.Bot.Name} opened inventory {args.Inventory.Type}.");
-    private void InventoryCloseHandler(InventoryCloseEventArgs args) => Debug.Info($"Bot {args.Bot.Name} closed inventory {args.Inventory.Type}.");
+    private void InventoryOpenHandler(InventoryOpenEventArgs args) =>
+        Debug.Info($"Bot {args.Bot.Name} opened inventory {args.Inventory.Type}.");
+
+    private void InventoryCloseHandler(InventoryCloseEventArgs args) =>
+        Debug.Info($"Bot {args.Bot.Name} closed inventory {args.Inventory.Type}.");
 
     private IEnumerable<IInstruction> TestJob()
     {
@@ -100,12 +110,12 @@ public class Program : SkyProgram
     private void IsOnGroundHandler(EntityIsOnGroundChangedEventArgs args) =>
         Debug.Info($"Entity is on ground changed: {args.Entity.Name}");
 
-    private void MoveCompleteHandler(BotNavigateCompletedEventArgs args)
+    private void MoveCompletedHandler(ITaskCompletedEventArgs<INavigateTask> args)
     {
-        var bot = args.Bot;
-        Debug.Info($"Bot {bot.Name} has completed moving: {args.Result}");
+        var bot = args.Task.Bot;
+        Debug.Info($"Bot {bot.Name} has completed moving: {args.Task.Result}");
         Debug.Info($"Interact {bot.Transform.Position}");
-        bot.InteractBlock(bot.Transform.Position );
+        bot.InteractBlock(bot.Transform.Position);
     }
 
     private void OnPositionChanged(EntityPositionChangedEventArgs args) =>
